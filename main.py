0b543e233cd61098c521cf5ce1f69041d3b0292b
Ricsius <hajnal.ricsi@gmail.com>
2024-11-20 21:35:12 +0100

    Add temperature plot with fake data

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import plotly.express as px
+
+def get_data(days):
+    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperatures = [10, 11, 15]
+    temperatures = [days * i for i in temperatures]
+
+    return dates, temperatures
 
 st.title("Weather Forecast for the next Days")
 
@@ -9,3 +17,8 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+dates, temperatures = get_data(days)
+figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+
+st.plotly_chart(figure)
